e8d1e22b1f9463115bcc15e57c7816bb528fcc01
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2020-04-04 11:11:50 +0200

    Print u(rho, T) and s(rho, T) too.

--- testscvheosreadtable.c
+++ testscvheosreadtable.c
@@ -20,9 +20,23 @@ int main(int argc, char **argv) {
     FILE *fp;
     int i, j;
 
-    printf("REOS3: Initializing material %i\n", iMat); 
+    printf("SCVHEOS: Initializing material %i\n", iMat); 
     Mat = scvheosInitMaterial(iMat, dKpcUnit, dMsolUnit);
     printf("Done.\n");
+
+    fprintf(stderr, "logT=");
+    for (i=0; i<Mat->nT; i++) {
+        fprintf(stderr, " %g", Mat->dLogTAxis[i]);
+    }
+    fprintf(stderr, "\n");
+    fprintf(stderr, "\n");
+
+    fprintf(stderr, "logRho=");
+    for (j=0; j<Mat->nRho; j++) {
+        fprintf(stderr, " %g", Mat->dLogRhoAxis[j]);
+    }
+    fprintf(stderr, "\n");
+    fprintf(stderr, "\n");
 
     printf("Writing EOS table to file\n");
 
@@ -35,6 +49,36 @@ int main(int argc, char **argv) {
         fprintf(fp, "%15.7E", Mat->dLogRhoAxis[j]);
         for (i=0; i<Mat->nT; i++) {
             fprintf(fp, "%15.7E", Mat->dLogPArray[i][j]);
+        }
+        
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+
+    /*
+     * Print u(rho) for different T.
+     */
+    fp = fopen("testscvheos_h_u_rho.txt", "w");
+
+    for (j=0; j<Mat->nRho; j++) {
+        fprintf(fp, "%15.7E", Mat->dLogRhoAxis[j]);
+        for (i=0; i<Mat->nT; i++) {
+            fprintf(fp, "%15.7E", Mat->dLogUArray[i][j]);
+        }
+        
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+
+    /*
+     * Print s(rho) for different T.
+     */
+    fp = fopen("testscvheos_h_s_rho.txt", "w");
+
+    for (j=0; j<Mat->nRho; j++) {
+        fprintf(fp, "%15.7E", Mat->dLogRhoAxis[j]);
+        for (i=0; i<Mat->nT; i++) {
+            fprintf(fp, "%15.7E", Mat->dLogSArray[i][j]);
         }
         
         fprintf(fp, "\n");
