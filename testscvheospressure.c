7cf6650099b4ddcfc0c56b030aaf5249426a08ba
Christian Reinhardt <christian.reinhardt@access.uzh.ch>
2020-04-06 19:56:00 +0200

    Added a program to test interpolation in pressure.

---
+++ testscvheospressure.c
@@ -0,0 +1,51 @@
+/*
+ * A simple program to test the SCVH EOS library.
+ *
+ * Author:   Christian Reinhardt
+ * Created:  06.04.2020
+ * Modified:
+ */
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+#include "scvheos.h"
+
+int main(int argc, char **argv) {
+	SCVHEOSMAT *Mat;
+    int iMat = SCVHEOS_H;
+    double dKpcUnit = 2.06701e-13;
+	double dMsolUnit = 4.80438e-08;
+    double logrho, logT;
+    FILE *fp;
+    int i, j;
+
+    dKpcUnit = 0.0;
+    dMsolUnit = 0.0;
+
+    printf("SCVH EOS: Initializing material %i\n", iMat); 
+    Mat = scvheosInitMaterial(iMat, dKpcUnit, dMsolUnit);
+    printf("Done.\n");
+
+    /* Interpolate in rho for differen isotherms. */
+    fp = fopen("testscvheospressure.txt", "w");
+
+    for (j=1; j<Mat->nRho-1; j++) {
+        logrho = 0.5*(Mat->dLogRhoAxis[j+1] + Mat->dLogRhoAxis[j]);
+        fprintf(fp, "%15.7E", logrho);
+
+        for (i=0; i<Mat->nT-1; i++) {
+            logT =  Mat->dLogTAxis[i];
+            fprintf(fp, "%15.7E", scvheosLogPofRhoT(Mat, logrho, logT));
+        }
+        
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
+
+    printf("Free memory\n");
+    scvheosFinalizeMaterial(Mat);
+    printf("Done.\n");
+
+    return 0;
+}
